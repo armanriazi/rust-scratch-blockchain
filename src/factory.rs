a4f4424039f1dc5c33790eb17fd34474af9f05b4
Arman Riazi <arman2riazi@gmail.com>
2022-06-27 20:20:44 +0000

    2022-27-06-1-ok

--- src/factory.rs
+++ src/factory.rs
@@ -20,34 +20,38 @@ pub fn blockchain_factory<F>(difficulty:u128, f : F) -> Result<(),CustomError>
     let blocks_val:serde_json::Value=serde_values_transactions["blocks"].clone();   
     let mut blockchain=Blockchain::new();
 
-
     blocks_val[0].as_object().unwrap().into_iter().enumerate().for_each(|(i, block)| {
 
-        //println!("\nBlock {:?}\n",block);
+        println!("\nBlock {:?}\n",block);
         block.1.as_array().unwrap().into_iter().enumerate().for_each(|(j, trxs)| {
         
         let transactions=trxs.get("transactions").unwrap();        
         let obg_trx=transactions.as_array().unwrap();                             
-        let trx=obg_trx[0].as_object().unwrap();        
-        let length=trx.keys().len()+1;   
-        
-        let maked_transaction:Vec<OptionTransaction>= fetch_raw_block_transactions(trx,length).unwrap();            
-        if maked_transaction.len()==0 {
-            return Err(CustomError::BlockchainFactory)
-        }       
+        let trx=obg_trx[0].as_object().unwrap();                
+        let length=&trx.keys().len()+1;   
+        println!("\n{:?}\n",trx);  
+
+        for c in 1..length{
+            let trx_name=concat_string!("transaction",c.to_string());
+            let trx=(transactions[0].as_object().unwrap()).get(&trx_name).unwrap();                
+            let puts=transaction_split(trx).unwrap();
+            println!("\n{:?}\n",puts);            
+        }        
     
-            // if i==1{
-            //     let mut genesis_block = Block::new(0, now(),vec![0; 32], maked_transaction, difficulty);    
-            //     prev_hash=genesis_block.mine().unwrap().into_boxed_slice();                
-            //     let _=&blockchain.update_with_block(genesis_block);                
-            // }
-            // else if i >1{
-            //     let mut maked_block:Block = Block::new(i as u32, now(), prev_hash.to_vec(), maked_transaction, difficulty);                                     
-            //     prev_hash=maked_block.mine().unwrap().into_boxed_slice();
-            //     let _=&blockchain.update_with_block(maked_block);
-            //     //println!("**maked_hash:**\n{:?}\n",&blockchain.blocks[i].prev_block_hash.clone());                
-            // }     
+     
       });   
+           
+    //   if i==1{
+    //         let mut genesis_block = Block::new(0, now(),vec![0; 32], maked_transaction, difficulty);    
+    //         prev_hash=genesis_block.mine().unwrap().into_boxed_slice();                
+    //         let _=&blockchain.update_with_block(genesis_block);                
+    //     }
+    //     else if i >1{
+    //         let mut maked_block:Block = Block::new(i as u32, now(), prev_hash.to_vec(), maked_transaction, difficulty);                                     
+    //         prev_hash=maked_block.mine().unwrap().into_boxed_slice();
+    //         let _=&blockchain.update_with_block(maked_block);
+    //         //println!("**maked_hash:**\n{:?}\n",&blockchain.blocks[i].prev_block_hash.clone());                
+    //     }    
    });
                                          
     Ok(()) 
@@ -79,7 +83,8 @@ fn transaction_split( trx:&serde_json::Value) -> Result<OptionTransaction,Custom
                 
                 if !item_internal_inputs.is_null(){
                 
-                    if !(item_internal_inputs["value"].is_null() && item_internal_inputs["to_addr"].is_null()){                      
+                    if !((item_internal_inputs["value"].is_null() && item_internal_inputs["to_addr"].is_null()) ||
+                     (item_internal_inputs["to_addr"].as_str().unwrap().is_empty() && item_internal_inputs["value"].as_str().unwrap().is_empty())){                      
                             trx_inputs_model= ModelValue{
                                 to_addr:item_internal_inputs["to_addr"].as_str().unwrap().to_owned(),
                                 value:item_internal_inputs["value"].as_str().unwrap().parse::<u64>().unwrap()
@@ -106,7 +111,8 @@ fn transaction_split( trx:&serde_json::Value) -> Result<OptionTransaction,Custom
                 
                 if !item_internal_outputs.is_null(){
                 
-                    if !(item_internal_outputs["value"].is_null() && item_internal_outputs["to_addr"].is_null()){                      
+                    if !((item_internal_outputs["value"].is_null() && item_internal_outputs["to_addr"].is_null())||
+                          item_internal_outputs["value"].as_str().unwrap().is_empty() && item_internal_outputs["to_addr"].as_str().unwrap().is_empty()){                      
                             trx_outputs_model= ModelValue{
                                 to_addr:item_internal_outputs["to_addr"].as_str().unwrap().to_owned(),
                                 value:item_internal_outputs["value"].as_str().unwrap().parse::<u64>().unwrap()
@@ -123,25 +129,4 @@ fn transaction_split( trx:&serde_json::Value) -> Result<OptionTransaction,Custom
         return Err(CustomError::BlockchainFactory) ;      
 }
 
-fn fetch_raw_block_transactions(transctions_map:Vec<serde_json::Value>,values_transactions_found_len:usize) -> Result<Vec<OptionTransaction>,CustomError>{
-   
-    for c in 1..length{
-        let trx_name=concat_string!("transaction",c.to_string());
-        let trx=(obg_trx[0].as_object().unwrap()).get(&trx_name).unwrap();
-        println!("\n{:?}\n",trx_name);
-        println!("\n{:?}\n",trx);
-
-        println!("\n--------------------------------------\n");
-    }
-    // let mut maked_transaction:Vec<OptionTransaction> = vec![];  
-    // println!("**j:**{:?}\n",&values_transactions_found_len);
-    // for j in 1..values_transactions_found_len+1{       
-    //     let transaction_str=concat_string!("transaction",j.to_string());
-    //     let transaction_map0=&transctions_map[0]["transactions"].clone();
-    //     let transaction_map1=&(transaction_map0[0].as_object().unwrap()).get_key_value(&transaction_str).unwrap();               
-    //     println!("**maked transactions:**\n{:?}\n\n",transaction_split(&transaction_map1.1).unwrap());      
-    //     let _=&maked_transaction.push(transaction_split(&transaction_map1.1).unwrap());            
-    // } 
-    Ok(maked_transaction)
-}
 
--- src/sample.rs
+++ src/sample.rs
@@ -4,84 +4,74 @@ use crate::CustomError;
 
 pub fn sample_trx_json_data_from_module() -> Result<serde_json::Value,CustomError>{
     
-     Ok(json!(
-        {
-            "blocks":[{    
-                "block1":[{
-                    "transactions":[{
-                        "transaction1":[
-                            {
-                                    "inputs":[{
-                                                    
-                                    }],    
-                                    "outputs":[{
-                                        "to_addr": "Alice",
-                                        "value": "50"                           
-                                    },{
-                                        "to_addr": "Bob",
-                                        "value": "10"                                         
-                                    }]    
-                                }
-                        ] ,
-                        "transaction2":[
-                            {
-                                    "inputs":[{                                                              
-                                    },{
-                                        "to_addr": "Alice",
-                                        "value": "50"                           
-                                    },{
-                                        "to_addr": "Bob",
-                                        "value": "10"                            
-                                    }],    
-                                    "outputs":[{       
-                                        "to_addr": "Mina",
-                                        "value": "48"                           
-                                    },{
-                                        "to_addr": "Nuha",
-                                        "value": "3"                                                          
-                                    }]    
-                                }
-                        ]                             
-                    }],
-                    "block2":[{
-                        "transactions":[{
-                            "transaction1":[
-                                {
-                                        "inputs":[{
-                                                        
-                                        }],    
-                                        "outputs":[{
-                                            "to_addr": "Alice",
-                                            "value": "10"                           
-                                        },{
-                                            "to_addr": "Bob",
-                                            "value": "0"                                         
-                                        }]    
-                                    }
-                            ] ,
-                            "transaction2":[
-                                {
-                                        "inputs":[{                                                              
-                                        },{
-                                            "to_addr": "Alice",
-                                            "value": "10"                           
-                                        },{
-                                            "to_addr": "Bob",
-                                            "value": "0"                            
-                                        }],    
-                                        "outputs":[{       
-                                            "to_addr": "Mina",
-                                            "value": "0"                           
-                                        },{
-                                            "to_addr": "Nuha",
-                                            "value": "0"                                                          
-                                        }]    
-                                    }
-                            ]                             
-                        }]                 
-                    }]
-                }]     
-             }] 
-            }        
-     ))
-}
+Ok(json!({
+"blocks":[{    
+    "block1":[{
+        "transactions":[{
+            "transaction1":[{
+                    "inputs":[{        
+                        "to_addr": "",
+                        "value": ""                               
+                    }],    
+                    "outputs":[{
+                        "to_addr": "Alice",
+                        "value": "50"                           
+                    },{
+                        "to_addr": "Bob",
+                        "value": "10"                                         
+                    }]    
+            }] ,
+            "transaction2":[{
+                    "inputs":[{                                                              
+                    },{
+                        "to_addr": "Alice",
+                        "value": "50"                           
+                    },{
+                        "to_addr": "Bob",
+                        "value": "10"                            
+                    }],    
+                    "outputs":[{       
+                        "to_addr": "Mina",
+                        "value": "48"                           
+                    },{
+                        "to_addr": "Nuha",
+                        "value": "3"                                                          
+                    }]    
+            }]                             
+        }]
+    }],
+    "block2":[{
+            "transactions":[{
+                "transaction1":[{
+                        "inputs":[{                           
+                        }],    
+                        "outputs":[{
+                            "to_addr": "Alice",
+                            "value": "10"                           
+                        },{
+                            "to_addr": "Bob",
+                            "value": "0"                                         
+                        }]    
+                }],
+                "transaction2":[{
+                        "inputs":[{                                                              
+                        },{
+                            "to_addr": "Alice",
+                            "value": "10"                           
+                        },{
+                            "to_addr": "Bob",
+                            "value": "0"                            
+                        }],    
+                        "outputs":[{       
+                            "to_addr": "Mina",
+                            "value": "0"                           
+                        },{
+                            "to_addr": "Nuha",
+                            "value": "232"                                                          
+                        }]    
+                }]                             
+            }]                 
+    }]      
+  }]
+})
+)}
